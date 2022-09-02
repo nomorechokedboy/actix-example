35a0285952569273a2b4b80a4ffeed7ffa152254
nomorechokedboy <haiduongit@protonmail.com>
2022-09-02 21:22:35 +0700

    feat: search for paging and fix typo

--- src/common/base_paging_query.rs
+++ src/common/base_paging_query.rs
@@ -3,5 +3,7 @@ use serde::Deserialize;
 #[derive(Debug, Deserialize)]
 pub struct BasePagingQuery {
     pub page: Option<u64>,
+    #[serde(rename = "pageSize")]
     pub page_size: Option<u64>,
+    pub search: Option<String>,
 }
--- src/posts/controller.rs
+++ src/posts/controller.rs
@@ -1,16 +1,25 @@
-use crate::common::app_state::AppState;
+use crate::common::{app_state::AppState, base_paging_query::BasePagingQuery};
 use actix_redis::{resp_array, Command, RespValue};
-use actix_web::{get, web, HttpRequest, HttpResponse};
+use actix_web::{
+    get,
+    web::{self, Query},
+    HttpRequest, HttpResponse,
+};
 use entity::post;
 use entity::post::Entity as Post;
-use sea_orm::{EntityTrait, PaginatorTrait, QueryOrder};
+use migration::Condition;
+use sea_orm::{ColumnTrait, EntityTrait, PaginatorTrait, QueryFilter, QueryOrder};
 
 #[get("/paging")]
-pub async fn get_posts(_req: HttpRequest, app_state: web::Data<AppState>) -> HttpResponse {
-    let page = 1;
-    // let page = params.page.unwrap_or(1);
-    let page_size = 20;
-    let cache_key = format!("products:paging:page={page}:pageSize={page_size}");
+pub async fn get_posts(
+    _req: HttpRequest,
+    query: Query<BasePagingQuery>,
+    app_state: web::Data<AppState>,
+) -> HttpResponse {
+    let page = query.page.unwrap_or(1);
+    let page_size = query.page_size.unwrap_or(20);
+    let search = query.search.clone().unwrap_or("".to_string());
+    let cache_key = format!("products:paging:page={page}:pageSize={page_size}:search={search}");
 
     let redis = &app_state.redis;
     let cache_res = redis
@@ -19,34 +28,34 @@ pub async fn get_posts(_req: HttpRequest, app_state: web::Data<AppState>) -> Htt
         .expect("Error getting post cache")
         .expect(format!("Can't get posts cache").as_str());
 
-    match cache_res {
-        RespValue::Nil => {
-            let db_con = &app_state.db_con;
-            let paginator = Post::find()
-                .order_by_asc(post::Column::Id)
-                .paginate(db_con, page_size.try_into().unwrap());
+    if let RespValue::BulkString(cache_string) = cache_res {
+        let posts: Vec<post::Model> =
+            serde_json::from_str(&String::from_utf8(cache_string).expect("Invalid UTF-8!"))
+                .unwrap();
 
-            let posts = paginator
-                .fetch_page((page - 1).try_into().unwrap())
-                .await
-                .expect("could not retrieve posts");
-            let cache = serde_json::to_string(&posts).unwrap();
-            redis
-                .send(Command(resp_array!["setex", cache_key, "300", cache]))
-                .await
-                .expect("Error setting posts cache")
-                .expect("Can't get cache set result");
-
-            HttpResponse::Ok().json(posts)
-        }
-
-        RespValue::BulkString(cache) => {
-            let posts: Vec<post::Model> =
-                serde_json::from_str(&String::from_utf8(cache).expect("Invalid UTF-8!")).unwrap();
-
-            HttpResponse::Ok().json(posts)
-        }
-
-        _ => HttpResponse::InternalServerError().json("Internal server error"),
+        return HttpResponse::Ok().json(posts);
     }
+
+    let db_con = &app_state.db_con;
+    let paginator = Post::find()
+        .filter(
+            Condition::any()
+                .add(post::Column::Title.like(format!("%{}%", &search).as_str()))
+                .add(post::Column::Text.like(format!("%{}%", &search).as_str())),
+        )
+        .order_by_asc(post::Column::Id)
+        .paginate(db_con, page_size.try_into().unwrap());
+
+    let posts = paginator
+        .fetch_page((page - 1).try_into().unwrap())
+        .await
+        .expect("could not retrieve posts");
+    let cache = serde_json::to_string(&posts).unwrap();
+    redis
+        .send(Command(resp_array!["setex", cache_key, "300", cache]))
+        .await
+        .expect("Error setting posts cache")
+        .expect("Can't get cache set result");
+
+    HttpResponse::Ok().json(posts)
 }
